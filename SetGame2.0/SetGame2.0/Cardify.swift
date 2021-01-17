40221e003d26df171dca5e4e315038e080db701b
loumelone <75758397+loumelone@users.noreply.github.com>
2021-01-17 08:30:34 -0500

    Creating Card's faces
    Began implementaion of the card features onto UI.

---
+++ SetGame2.0/SetGame2.0/Cardify.swift
@@ -0,0 +1,37 @@
+//
+//  Cardify.swift
+//  Memorize2.0
+//
+//  Created by Louis Melone on 12/29/20.
+//
+
+
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    //This ViewModifier takes some 'content' and puts it on a card
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+            RoundedRectangle(cornerRadius: cornerRadius).stroke(Color.black, lineWidth: edgeLineWidth)
+            content
+            }
+        .aspectRatio(0.75, contentMode: .fit)
+        .padding()
+        }
+
+    private let cornerRadius: CGFloat = 10
+    private let edgeLineWidth: CGFloat = 3
+    
+}
+
+extension View {
+    func cardify() -> some View {
+        self.modifier(Cardify())
+    }
+}
+    
+ 
+
--- SetGame2.0/SetGame2.0/SetGameView.swift
+++ SetGame2.0/SetGame2.0/SetGameView.swift
@@ -14,7 +14,7 @@ struct SetGameView: View {
     var body: some View {
         VStack{
             Grid(SetVM.faceUpCards) { card in
-                CardView().onTapGesture {
+                CardView(card: card).onTapGesture {
                     SetVM.choose(card: card)
                 }
             }
@@ -31,15 +31,56 @@ struct SetGameView: View {
 }
 
 struct CardView: View {
+    var card: Game.Card
+    
+    var opacity: Double {
+        if card.shade == .dark{ return 1 }
+        else if card.shade == .light { return 0.5 }
+        else { return 0 }
+    }
+    
+    var color: Color {
+        if card.color == .blue { return Color.blue }
+        else if card.color == .green { return Color.green }
+        else { return Color.red }
+    }
+    
+    var count: Int {
+        return card.count
+    }
     
     var body: some View {
-        RoundedRectangle(cornerRadius: 10)
-            .fill(Color.blue)
-            .padding()
-            .aspectRatio(0.75, contentMode: .fit)
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
         }
-    
+    }
+        @ViewBuilder
+        private func body(for size: CGSize) -> some View {
+            VStack {
+                ForEach(0..<count) { index in
+                    ZStack{
+                        if card.shape == .rect {
+                            Rectangle().fill(color).opacity(opacity)
+                            Rectangle().stroke(color)
+
+                        }
+                        if card.shape == .capsule {
+                            Capsule().fill(color).opacity(opacity)
+                            Capsule().stroke(color)
+                        }
+                        else {
+                            Circle().fill(color).opacity(opacity)
+                            Circle().stroke(color)
+                        }
+                    }
+                    
+                }
+            }
+            .cardify()
+
+    }
 }
+
 
 
 
